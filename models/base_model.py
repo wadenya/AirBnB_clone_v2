cb81cc54c87c0c7468546627c92dd1154319a477
Castro Hamiltone <castromiltone@gmail.com>
2023-08-21 22:32:28 +0300

    fix

--- models/base_model.py
+++ models/base_model.py
@@ -25,11 +25,10 @@ class BaseModel:
         default=datetime.utcnow)
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-        else:
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        if kwargs:
             for k, v in kwargs.items():
                 if k == "created_at" or k == "updated_at":
                     setattr(self, k, datetime.fromisoformat(v))
