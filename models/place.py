f715ded622da4f1da1ff773f4a4938d684d15261
wadenya <alexwadenya001@gmail.com>
2023-08-21 03:43:16 -0700

    DBStorage - User

--- models/place.py
+++ models/place.py
@@ -4,7 +4,7 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, Float, ForeignKey
 
 
-class Place(BaseModel):
+class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ == 'places';
     city_id = Column(String(60), Foreignkey('Cities.id'), nullable=False)
