9d8d10ccb9b83e246c0084798a284740b266471e
Castro Hamiltone <castromiltone@gmail.com>
2023-08-21 23:12:20 +0300

    fix

--- models/review.py
+++ models/review.py
@@ -5,7 +5,7 @@ from models.base_model import BaseModel, Base
 
 class Review(BaseModel, Base):
     """ Review classto store review information """
-    __tabelname__ = 'reviews'
+    __tablename__ = 'reviews'
     place_id = ""
     user_id = ""
     text = ""
