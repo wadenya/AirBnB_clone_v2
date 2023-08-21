9c91841b9b347c038630f08af07c483c2d915720
Castro Hamiltone <castromiltone@gmail.com>
2023-08-21 21:05:19 +0300

    fix

--- models/base_model.py
+++ models/base_model.py
@@ -22,7 +22,7 @@ class BaseModel:
     updated_at = Column(
         DateTime,
         nullable=False,
-        default=datetime.datetime.utcnow)
+        default=datetime.utcnow)
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
