83274d6c6d4148e1321e2ece12af3f4757689662
Castro Hamiltone <castromiltone@gmail.com>
2023-08-21 22:02:08 +0300

    fix

--- models/base_model.py
+++ models/base_model.py
@@ -30,12 +30,11 @@ class BaseModel:
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    setattr(self, k, datetime.fromisoformat(v))
+                elif k != "__class__":
+                    setattr(self, k, v)
 
     def __str__(self):
         """Returns a string representation of the instance"""
