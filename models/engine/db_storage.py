5e86f89eb1620fafa60b0ffcc27c2838dba4fbcd
Castro Hamiltone <castromiltone@gmail.com>
2023-08-21 21:02:16 +0300

    db storage - init, all

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -2,6 +2,9 @@
 """module defines DBStorage class"""
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
+from os import getenv
+
+from models.base_model import BaseModel, Base
 
 
 class DBStorage:
@@ -11,5 +14,35 @@ class DBStorage:
 
     def __init__(self) -> None:
         """creates the engine"""
-        self.__engine = create_engine()
+        conn_str = 'mysql+mysqldb://{}:{}@{}/{}'.format(
+            getenv('HBNB_MYSQL_USER'),
+            getenv('HBNB_MYSQL_PWD'),
+            getenv('HBNB_MYSQL_HOST'),
+            getenv('HBNB_MYSQL_DB'))
+        self.__engine = create_engine(conn_str, pool_pre_ping=True)
+        if getenv("HBNB_ENV") == "test":
+            Base.metadata.drop_all(self.__engine)
 
+    def all(self, cls=None):
+        """get all objects in db based on cls"""
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.place import Place
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.review import Review
+
+        classes = {
+                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
+                    'State': State, 'City': City, 'Amenity': Amenity,
+                    'Review': Review
+                  }
+        if cls is not None:
+            objs = {}
+            objs[cls] = self.__session.query(classes[cls]).all()
+        else:
+            for _cls in classes:
+                objs = {}
+                objs[_cls] = self.__session.query(classes[_cls]).all()
+        return {"{}.{}".format(type(obj).__name__, obj.id): obj for obj in objs}
