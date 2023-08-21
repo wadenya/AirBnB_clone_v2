b1d5ceafafd8fbfe3ef9c1de79bec434996bb6aa
Castro Hamiltone <castromiltone@gmail.com>
2023-08-21 23:33:28 +0300

    fix

--- models/place.py
+++ models/place.py
@@ -7,7 +7,7 @@ from sqlalchemy import Column, String, Integer, Float, ForeignKey
 class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = 'places';
-    city_id = Column(String(60), ForeignKey('Cities.id'), nullable=False)
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     name = Column(String(128), nullable=False)
     description = Column(String(1024), nullable=True)
