b7b2e7a72d9c2e7a27cba46ba13f0213b8aaa9b3
wadenya <alexwadenya001@gmail.com>
2023-08-21 00:55:18 -0700

    MySQL setup test

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -7,5 +7,8 @@ CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 -- Grant all privileges on hbnb_test_db to hbnb_test
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 
+-- Grant SELECT privileges on performance_schema to hbnh_test
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+
 -- Flush the privileges to ensure they take effect
 FLUSH PRIVILEGES;
