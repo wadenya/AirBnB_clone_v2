81e4f73a545795e2fe325c18800044e4e782567c
wadenya <alexwadenya001@gmail.com>
2023-08-23 00:09:10 -0700

    DBStorage - Amenity... and BOOM!

--- models/amenity.py
+++ models/amenity.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
 
 
 class Amenity(BaseModel, Base):
     __tablename__ = 'amenities'
-    name = ""
+    name = Column(String(128), nullable=False)
