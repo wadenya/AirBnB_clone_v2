59ee167b1c0a98f5c03f1864abc37c50fbf0fa27
Castro Hamiltone <castromiltone@gmail.com>
2023-08-23 18:22:02 +0300

    fix - place.py

--- models/place.py
+++ models/place.py
@@ -6,13 +6,12 @@ from models.review import Review
 from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 from sqlalchemy.orm import relationship
 
-association_table = Table("place_amenity",
+association_table = Table("place_amenity", Base.metadata,
                           Column("place_id", String(60),
                                  ForeignKey("places.id"),
                                  primary_key=True, nullable=False),
                           Column("amenity_id", String(60),
                                  ForeignKey("amenities.id"),
-                                 metadata=Base.metadata,
                                  primary_key=True, nullable=False))
 
 class Place(BaseModel, Base):
