b51a196cb2815a9c764747acf7781d8d36bf1245
Castro Hamiltone <castromiltone@gmail.com>
2023-08-23 13:58:25 +0300

    bug fix

--- models/place.py
+++ models/place.py
@@ -3,9 +3,16 @@
 from models.base_model import BaseModel, Base
 from models.amenity import Amenity
 from models.review import Review
-from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 from sqlalchemy.orm import relationship
 
+association_table = Table("place_amenity", Base.metadata,
+                          Column("place_id", String(60),
+                                 ForeignKey("places.id"),
+                                 primary_key=True, nullable=False),
+                          Column("amenity_id", String(60),
+                                 ForeignKey("amenities.id"),
+                                 primary_key=True, nullable=False))
 
 class Place(BaseModel, Base):
     """ A place to stay """
