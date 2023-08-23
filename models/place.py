70401973efd5c25cb17fffd2619b1341e691c391
Castro Hamiltone <castromiltone@gmail.com>
2023-08-23 18:26:52 +0300

    fix - place.py

--- models/place.py
+++ models/place.py
@@ -31,7 +31,7 @@ class Place(BaseModel, Base):
     longitude = Column(Float, nullable=True)
     reviews = relationship("Review", backref="place", cascade="delete")
     amenities = relationship("Amenity", secondary="place_amenity",
-                             viewonly=False, back_populates="amenities")
+                             viewonly=False, back_populates="place_amenities")
     amenity_ids = []
 
     if getenv("HBNB_TYPE_STORAGE") != "db":
