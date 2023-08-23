859a0b6fae36d9fc7621244328086d8112a4f2dc
wadenya <alexwadenya001@gmail.com>
2023-08-23 01:46:33 -0700

    DBStorage - Amenity... and BOOM!

--- models/amenity.py
+++ models/amenity.py
@@ -2,8 +2,13 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
     __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
+
+    # Many-To-Many relationship with Place
+    place_amenities = relationship("Place", secondary="place_amenity",
+                                    viewonly=False)
