19a2bad08c90ea2eac2e7beb3fdbe19d7aed79b1
wadenya <alexwadenya001@gmail.com>
2023-09-08 05:36:02 +0000

    test

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,24 +1,19 @@
 #!/usr/bin/python3
+"""Compress before sending"""
+
 from fabric.api import local
 from datetime import datetime
+from fabric.decorators import runs_once
 
-def do_pack():
-    """
-    Packs web_static files into .tgz file.
-    """
-    
-    # Get the current time in the format yearmonthdayhourminutesecond
-    time_now = datetime.now().strftime('%Y%m%d%H%M%S')
-    archive_name = "web_static_{}.tgz".format(time_now)
-    archive_path = "versions/{}".format(archive_name)
+@runs_once
+def create_archive():
+    '''Package web_static directory into timestamped .tgz archive'''
+    local("mkdir -p archives")
+    archive_path = ("archives/content_{}.tgz"
+                    .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")))
+    tar_command = local("tar -cvzf {} web_static"
+                        .format(archive_path))
 
-    # Create directory if it doesn't exist
-    local("mkdir -p versions")
-
-    # Create the tarball
-    archive_command = local("tar -cvzf {} web_static".format(archive_path))
-
-    # Check if the command was successful
-    if archive_command.succeeded:
-        return archive_path
-    return None
+    if tar_command.failed:
+        return None
+    return archive_path
