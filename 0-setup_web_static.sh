c85181ad6fecd6630feeac492a9ecbcc37ccf837
wadenya <alexwadenya001@gmail.com>
2023-09-08 06:12:22 +0000

    test 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -14,12 +14,12 @@ echo "Holberton School" > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 #Ownership to ubuntu
-sudo chown -hR ubuntu:ubuntu /data/
+chown -R ubuntu /data/
+chgrp -R ubuntu /data/
 
 printf %s "server {
     listen 80 default_server;
     listen [::]:80 default_server;
-    server_name zanderlex.tech;
     add_header X-Served-By $HOSTNAME;
     root   /var/www/html;
     index  index.html index.htm;
@@ -27,10 +27,14 @@ printf %s "server {
         alias /data/web_static/current;
         index index.html index.htm;
     }
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
     error_page 404 /404.html;
     location /404 {
       root /var/www/html;
       internal;
     }
 }" > /etc/nginx/sites-available/default
-sudo service nginx restart
+
+service nginx restart
