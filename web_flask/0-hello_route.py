344986b0d999cabc89d07cb6f3d8f78dbe84ea1d
wadenya <alexwadenya001@gmail.com>
2023-09-22 11:39:32 +0000

    all

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,14 +4,14 @@
 
 from flask import Flask
 
-app = Flask(__name__)
+myapp = Flask(__name__)
 
 #define route for root url
-@app.route("/", strict_slashes=False)
+@myapp.route("/", strict_slashes=False)
 def hello_hbnb():
     """Displays 'Hello HBNB!'"""
     return "Hello HBNB!"
 
 if __name__ == "__main__":
 	#listen on all available network interface 0.0.0.0 and port 5000
-	app.run(host='0.0.0.0', port=5000)
+	myapp.run(host='0.0.0.0', port=5000)
